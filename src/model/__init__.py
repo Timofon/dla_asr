508f7064e0c9f5e223ea904b67ed2bebe25d189b
Timofei Senin <tpsenin@gmail.com>
2024-10-10 19:48:10 +0300

    change model to conformer

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,10 +1,9 @@
 from src.model.baseline_model import BaselineModel
-
-# from src.model.conformer.conformer_block.conformer import Conformer
+from src.model.conformer.conformer import Conformer
 from src.model.deepspeech.deepspeech2 import DeepSpeech2
 
 __all__ = [
     "BaselineModel",
-    # "Conformer",
+    "Conformer",
     "DeepSpeech2",
 ]
